c835efedf7a4518a45325f55a29fff2b84a18f1b
Jeewon Yang <jwyang.ai@gmail.com>
2023-01-29 20:07:24 +0900

    Delete 5_compare_hic_450k.sh

--- scripts/5_compare_hic_450k.sh
+++
@@ -1,9 +0,0 @@
-chrom=14
-w_dir=/data/project/jeewon/research/3dith-reproduce
-d_dir=/data/project/jeewon/research/3dith-reproduce/data
-s_dir=/data/project/jeewon/research/3dith-reproduce/result
-cpg_type=opensea
-grch=36
-
-echo "python3 5_compare_hic_450K.py --chrom $chrom -w_dir $w_dir -d_dir $d_dir -s_dir $s_dir --cpg_type $cpg_type --grch $grch"
-python3 5_compare_hic_450k.py --chrom $chrom -w_dir $w_dir -d_dir $d_dir -s_dir $s_dir --cpg_type $cpg_type --grch $grch
